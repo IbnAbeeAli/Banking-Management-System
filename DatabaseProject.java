1d3fc3b46e96569078b57b093586c8d943a1927a
IbnAbeeAli <76901480+IbnAbeeAli@users.noreply.github.com>
2021-12-31 18:17:02 +0500

    Add files via upload

---
+++ DatabaseProject.java
@@ -0,0 +1,41 @@
+package databaseproject;
+
+import java.sql.*;
+import java.util.Scanner;
+
+
+public class DatabaseProject {
+    
+
+//
+//   public static void main(String[] args) throws Exception {
+//
+//       // Scanner Object for taking the password
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter the password for the database: ");
+//        //password of this connection is: seecs@123
+//        //user and password of the database
+//        String user = "root";
+//        //taking input from the user
+//        String password = scanner.next();
+//
+//        //Getting connection to the database
+//        Connection myCon = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", user, password);
+//        // Creating a statement
+//        Statement myStmt = myCon.createStatement();
+//            
+//        
+//        System.out.println(sql);
+//        //Execution of sql query
+//        ResultSet myRs = myStmt.executeQuery(sql);
+//        //Printing the 
+//        //System.out.println("Tables_in_project");
+//
+//        //Processing the results
+//        while (myRs.next()) {
+//            System.out.println(myRs.getString("Tables_in_project"));
+//        }
+//
+//    }
+    
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package databaseproject;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainFrame obj = new mainFrame();
+        obj.setVisible(true);
+
+    }
+
+}
